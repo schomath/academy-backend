0626ffa3d5896c8450ba8ff0e339e2f345c7fba0
Mathias Schoen <schomath@gmail.com>
2026-03-01 02:01:41 -0800

    trying full path

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // base: '/fm-academy/',
+      base: 'https://matisha.github.io/academy-backend/',
       server: {
         port: 3000,
         host: '0.0.0.0',
